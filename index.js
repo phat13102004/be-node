a02bb477aff67ecdb28c3bc518d5b7206f9d1f08
Tran Tuan Phat <trantuanphat5392@gmail.com>
2025-12-15 15:46:20 +0700

    code

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Backend NodeJS is running" });
+  res.json({ message: "Trần Tuấn Phát-Cuối Kỳ" });
 });
 
 const PORT = process.env.PORT || 3000;
